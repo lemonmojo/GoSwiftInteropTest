ba2934ed226597604ab1cf224691fccd6ae6d8f6
Felix Deimel <felix@lemonmojo.com>
2024-01-07 20:01:15 +0100

    Debug logs

--- Swift/UpperKit/Sources/UpperKit/Logger.swift
+++ Swift/UpperKit/Sources/UpperKit/Logger.swift
@@ -3,9 +3,15 @@ import Foundation
 public class Logger {
     public static let shared = Logger()
     
-    public init() { }
+    public init() {
+        log("[DEBUG] Initializing Logger")
+    }
     
     public func log(_ message: String) {
         print(message)
+    }
+    
+    deinit {
+        log("[DEBUG] Deinitializing Logger")
     }
 }
